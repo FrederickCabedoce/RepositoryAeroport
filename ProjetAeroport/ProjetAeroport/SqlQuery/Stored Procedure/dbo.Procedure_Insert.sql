f7114222f989b7c61c528a1f73ed55a6eb85d8f7
Frédérick Cabedoce <Frederick.Cabedoce@hotmail.com>
2018-09-03 19:41:09 -0700

    Small changes
    ayelmao

---
+++ ProjetAeroport/ProjetAeroport/SqlQuery/Stored Procedure/dbo.Procedure_Insert.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [dbo].[Insert_Vol]
+	@ID varchar(7),
+	@DESTINATION varchar(30),
+	@ETAT varchar(10),
+	@LOGO varchar(75),
+	@PREVU varchar(10),
+	@REVISE varchar(10),
+	@PORTE varchar(5),
+	@DATE datetime,
+	@TYPEVOL varchar(20)
+	
+AS
+	insert into [dbo].[Vol](
+	[ID],
+	[DESTINATION],
+	[ETAT],
+	[LOGO],
+	[PREVU],
+	[REVISE],
+	[PORTE],
+	[DATE],
+	[TYPEVOL]
+	) values (
+	@ID,
+	@DESTINATION,
+	@ETAT,
+	@LOGO,
+	@PREVU,
+	@REVISE,
+	@PORTE,
+	@DATE,
+	@TYPEVOL
+	)
+go
---
+++ ProjetAeroport/ProjetAeroport/SqlQuery/Stored Procedure/dbo.Procedure_Select.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].SelectVol
+	@TYPEVOL varchar,
+	@DATE datetime
+	
+AS
+	SELECT * from [dbo].Vol where TYPEVOL = @TYPEVOL and DATE = @DATE;
+	go
